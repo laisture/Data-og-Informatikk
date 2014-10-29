8fb369ac1077a551abd3f954d6e45dee2aee9d8d
laisture <jqlle_kqlle@hotmail.com>
2014-10-29 13:02:18 +0100

    Create Template.php

---
+++ Template.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<link type="text/css" rel="stylesheet" href="oblig2.css"/>
+		<title>Julian Website</title>
+	</head>
+	<body>
+		<div id="main">
+				<nav>
+					<a href="index.php">Home</a>
+					<a href="pictures.php">Pictures</a>
+					<a href="videos.php">Videos</a>
+					<a href="about.php">About</a>
+				</nav>
+		</div>
+	</body>
+	<footer>	</footer>
+</html>
