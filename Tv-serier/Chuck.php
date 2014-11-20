cff0d9de0e6f2332f2ca7e99bfe3f088d69b9fcf
Lavanja <lavanja.jeyen@yahoo.no>
2014-11-20 20:53:29 +0100

    Create Chuck.php

---
+++ Tv-serier/Chuck.php
@@ -0,0 +1,39 @@
+<?php
+
+	$title = "Chuck"; // Here goes the title of the tv-serie.
+
+	$summary = "When a twnty-something computer geek invadertently downloads critical government secrets into his brain, CIA and NSA assign two agents to protect him and exploit such knowledge, turning his life upside down."; // Here goes the summary of the tv-serie.
+
+	$poster ="http://nerdreactor.com/wp-content/uploads/2014/05/Chuck-Season-5-Promotional-Picture-NEW.jpg"; // Here goes the url for the poster of the tv-serie.
+
+	$altPoster ="The poster of Chuck"; // Here goes the alt text for the poster picture.
+
+	$name1 = "Bjarne"; // Here goes the name of the the first person who wrote a review.
+
+	$date1 = "May 25 2011"; // Here goes the date of the first review.
+
+	$stars1 = "4-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
+
+	$altStars1 = "4 out of 5 stars rating"; // Here goes the alt text for the first stars picture.
+
+	$review1 = "I Love this Show because it has some Computer grade hummor init and would be interesting if Humans could get a computer downloaded in to a person.I hope Morgan does continue with being the Intersect with CHUCK. being the Intersect along side each other..."; // Here goes the first review text.
+
+	$name2 = "Beate"; // Here goes the name of the the second person who wrote a review.
+
+	$date2 = "Dec 21 2013"; // Here goes the date of the second review.
+
+	$stars2 = "2-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
+
+	$altStars2 = "2 out of 5 stars rating"; // Here goes the alt text for second the stars picture.
+
+	$review2 = "I have been told this show is funny and entertaining. However, after only watching the first episode, I can tell it is a mindless, cliched, and predictable drama. When the episode began, I was interested and curious to find out what would happen, but after I saw all the sexist stereotyping of the Sarah character, I was instantly disappointed. Why the writers chose to create such an unlikeable character, I will never know, but knowing the show will probably revolve around a naked blond, I would rather watch anything else."; // Here goes the second review text.
+
+
+	include 'template_header.php';
+
+?>
+
+<?php
+
+	include 'template_footer.php';
+?>
