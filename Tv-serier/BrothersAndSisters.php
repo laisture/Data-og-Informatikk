b638cfad529e8d07c16eef6ee3a028704906ed00
Lavanja <lavanja.jeyen@yahoo.no>
2014-11-20 20:50:29 +0100

    Create BrothersAndSisters.php

---
+++ Tv-serier/BrothersAndSisters.php
@@ -0,0 +1,39 @@
+<?php
+
+	$title = "Brothers And Sisters"; // Here goes the title of the tv-serie.
+
+	$summary = "The story of the dama surrounding a family´s adult siblings."; // Here goes the summary of the tv-serie.
+
+	$poster ="http://ecx.images-amazon.com/images/I/51N2WOAAWjL.jpg"; // Here goes the url for the poster of the tv-serie.
+
+	$altPoster ="The poster of Brothers and Sisters"; // Here goes the alt text for the poster picture.
+
+	$name1 = "Josefine"; // Here goes the name of the the first person who wrote a review.
+
+	$date1 = "Oct 28 2008"; // Here goes the date of the first review.
+
+	$stars1 = "4-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
+
+	$altStars1 = "4 out of 5 stars rating"; // Here goes the alt text for the first stars picture.
+
+	$review1 = "An amazing drama, from the kind we don't have any more on TV. A great acting from all the cast and well scripted show. I cant wait for the next episode, especially now that Justin and Rebbecca are together."; // Here goes the first review text.
+
+	$name2 = "Halfdan"; // Here goes the name of the the second person who wrote a review.
+
+	$date2 = "May 22 2007"; // Here goes the date of the second review.
+
+	$stars2 = "5-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
+
+	$altStars2 = "5 out of 5 stars rating"; // Here goes the alt text for second the stars picture.
+
+	$review2 = "I am absolutely in love with this show. The Walker family is one to watch, see how they grow and love each other through everything. Witty, emotional, and real."; // Here goes the second review text.
+
+
+	include 'template_header.php';
+
+?>
+
+<?php
+
+	include 'template_footer.php';
+?>
