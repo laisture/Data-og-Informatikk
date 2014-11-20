b03f80f6b4288bff95e7645b350f47aebf981297
Lavanja <lavanja.jeyen@yahoo.no>
2014-11-20 20:59:18 +0100

    Create Gotham.php

---
+++ Tv-serier/Gotham.php
@@ -0,0 +1,39 @@
+<?php
+
+	$title = "Gotham"; // Here goes the title of the tv-serie.
+
+	$summary = "The story behind Commissioner James Gordon's rise to prominence in Gotham City in the years before Batman's arrival."; // Here goes the summary of the tv-serie.
+
+	$poster = "http://serialgotham.ru/uploads/posts/2014-08/1407391435_serialgotham.ru_tumblr_n9gjtdnqzl1t0qyojo1_1280.png"; // Here goes the url for the poster of the tv-serie.
+
+	$altPoster ="The poster of Gotham"; // Here goes the alt text for the poster picture.
+
+	$name1 = "Einar"; // Here goes the name of the the first person who wrote a review.
+
+	$date1 = "Sep 25 2014"; // Here goes the date of the first review.
+
+	$stars1 = "5-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
+
+	$altStars1 = "5 out of 5 stars rating"; // Here goes the alt text for the first stars picture.
+
+	$review1 = "The writers and crews who work on the pilot for Gotham must be a genius. The production is next to flawless and it clearly shows how huge the show can be (only if it keeps on taking the right direction) and the tremendous potential the characters and stories in the Batman/DC universe carry. Definitely a game-changer for Fox. Who needs originality when you have authenticity blended with vision!"; // Here goes the first review text.
+
+	$name2 = "Martin"; // Here goes the name of the the second person who wrote a review.
+
+	$date2 = "Oct 11 2014"; // Here goes the date of the second review.
+
+	$stars2 = "3-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
+
+	$altStars2 = "3 out of 5 stars rating"; // Here goes the alt text for second the stars picture.
+
+	$review2 = "This show disgusts me. I knew it would be bad but how bad I could not fathom. It has no redeeming features and I really feel bad for people that made it and watch it. I could barely stomach the first episode and couldn't wait for it to be over. I have since seen two other scenes and put it up there with the worst shows on tv, The acting is bad but the writing is unforgivable. "; // Here goes the second review text.
+
+
+	include 'template_header.php';
+
+?>
+
+<?php
+
+	include 'template_footer.php';
+?>
