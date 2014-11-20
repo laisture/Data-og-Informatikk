a97db2f98c3c2275ad4a46cfbc9bc59a740cde2f
Lavanja <lavanja.jeyen@yahoo.no>
2014-11-20 20:56:19 +0100

    Create DaysOfOurLives.php

---
+++ Tv-serier/DaysOfOurLives.php
@@ -0,0 +1,39 @@
+<?php
+
+	$title = "Days Of Our Lives"; // Here goes the title of the tv-serie.
+
+	$summary = "A chronicle of the lives, loves, trials and tribulations of the citizens of the fictional city of Salem."; // Here goes the summary of the tv-serie.
+
+	$poster ="http://archive.wkyc.com/weblog/directors_cut/uploaded_images/dayslogo-771984.jpg"; // Here goes the url for the poster of the tv-serie.
+
+	$altPoster ="The poster of Days Of Our Lives"; // Here goes the alt text for the poster picture.
+
+	$name1 = "Eirin"; // Here goes the name of the the first person who wrote a review.
+
+	$date1 = "Oct 27 2012"; // Here goes the date of the first review.
+
+	$stars1 = "3-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
+
+	$altStars1 = "3 out of 5 stars rating"; // Here goes the alt text for the first stars picture.
+
+	$review1 = "I have been watching Days for 50 years!! Yes. My Grandma and I watched it when I was a kid. I've seen many characters come and go. This has to be the worst cast ever. New people every week. Older characters dumped. It's very sad to me. I really wish that Daniel would get together with Jen! I find the whole show frustrating. I realize that you need younger viewers, but what thetlpeople who have been watching forever? I work full-time, so I usually have to catch up. I knew Sammy was leaving, but E J? Then old Dimera goes on and done. I'll miss my old Suzanne;" // Here goes the first review text.
+
+	$name2 = "Leo"; // Here goes the name of the the second person who wrote a review.
+
+	$date2 = "Feb 14 2013"; // Here goes the date of the second review.
+
+	$stars2 = "3-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
+
+	$altStars2 = "3 out of 5 stars rating"; // Here goes the alt text for second the stars picture.
+
+	$review2 = "I am so unhappy with the days of our lives writers. What happened to John and it showing it in what the broad did and then he said, she was innocent that it was him, being in a rage. You guys really suck. You don't want to write the idiot woman out of the show. I have watched this show for 15 years and I am finished. If you can't write any better than this then I am finished watching this show."; // Here goes the second review text.
+
+
+	include 'template_header.php';
+
+?>
+
+<?php
+
+	include 'template_footer.php';
+?>
