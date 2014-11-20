d463ae9d568e8cea7199bcd06454265157d31aad
Lavanja <lavanja.jeyen@yahoo.no>
2014-11-20 21:00:08 +0100

    Create GreysAnatomy.php

---
+++ Tv-serier/GreysAnatomy.php
@@ -0,0 +1,39 @@
+<?php
+
+	$title = "Grey´s Anatomy"; // Here goes the title of the tv-serie.
+
+	$summary = "A drama centeres on the personal and professional lives of five surgical interns and their supervisors."; // Here goes the summary of the tv-serie.
+
+	$poster ="http://www.hollywoodreporter.com/sites/default/files/custom/LESLEY/Greys_Anatomy_Key_Art_Blog_Embed.jpg"; // Here goes the url for the poster of the tv-serie.
+
+	$altPoster ="The poster of Grey´s Anatomy"; // Here goes the alt text for the poster picture.
+
+	$name1 = "Lars"; // Here goes the name of the the first person who wrote a review.
+
+	$date1 = "Jan 10 2011"; // Here goes the date of the first review.
+
+	$stars1 = "5-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
+
+	$altStars1 = "5 out of 5 stars rating"; // Here goes the alt text for the first stars picture.
+
+	$review1 = "Great show. Makes me laugh and cry each episode. The story line always leaves me shocked and curious. Can't wait till the finale to see how it all ends for this season. You go Shonda Rhimes and the rest of the crew. I love you all, continue your great work! "; // Here goes the first review text.
+
+	$name2 = "Henrik"; // Here goes the name of the the second person who wrote a review.
+
+	$date2 = "Sep 21 2012"; // Here goes the date of the second review.
+
+	$stars2 = "4-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
+
+	$altStars2 = "4 out of 5 stars rating"; // Here goes the alt text for second the stars picture.
+
+	$review2 = "The show has had potential from its beginning and has shown an admireable ability to quickly develop complex characters. Sandra Oh's playing of Yang brings a very funny atmosphere to the team with her twisted sense of humour and innapropriate behaviour. The complicated relationship between Derek and Meredith can be fun, but also annoying at times."; // Here goes the second review text.
+
+
+	include 'template_header.php';
+
+?>
+
+<?php
+
+	include 'template_footer.php';
+?>
