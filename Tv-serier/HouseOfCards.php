bef55ea33efd5cf7361b0b43b180480c54120cfa
Lavanja <lavanja.jeyen@yahoo.no>
2014-11-20 20:52:09 +0100

    Create HouseOfCards.php

---
+++ Tv-serier/HouseOfCards.php
@@ -0,0 +1,34 @@
+<?php
+
+	$title = "House Of Cards"; // Here goes the title of the tv-serie.
+
+	$summary = "A Congressman works with his equally conniving wife to exact revenge on the people who betrayed him."; // Here goes the summary of the tv-serie.
+
+	$poster ="http://img4.wikia.nocookie.net/__cb20140217231358/house-of-cards/images/a/a8/House_of_Cards_Season_1_Poster.jpg"; // Here goes the url for the poster of the tv-serie.
+	
+	$altPoster ="The poster of House Of Cards"; // Here goes the alt text for the poster picture.
+
+	$name1 = "Emily,"; // Here goes the name of the the first person who wrote a review.
+
+	$date1 = "Apr 27 2014"; // Here goes the date of the first review.
+
+	$stars1 = "5-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
+	
+	$altStars1 = "5 out of 5 stars rating"; // Here goes the alt text for the first stars picture.
+
+	$review1 = "Very little shows prove how much cinema quality is needed on TV as this one. Many series mock movies, this IS a sort of long movie that gets better each episode. Season 1 of House of Cards is meant to watch at your own pace, falling in love with its incredible performances, rock-solid direction and sharp, incredible writing. Is a series that offers something different in terms of soapiness in comparison to other TV Shows. It is a smart Scorsesenian political satire, and is so damn entertaining and addictive you swear you will describe the show as an incredibly powerful drug. Kevin "; // Here goes the first review text.
+
+	$name2 = "Barky,"; // Here goes the name of the the second person who wrote a review.
+
+	$date2 = "Sept 21 2013"; // Here goes the date of the second review.
+
+	$stars2 = "5-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
+	
+	$altStars2 = "5 out of 5 stars rating"; // Here goes the alt text for second the stars picture.
+
+	$review2 = "A dark and cynical take on Washington with an incredible score, first-rate production values, and excellent performances (particularly by the female cast). What keeps it from being better is that it is occasionally let down by heavy-handed and hammy dialogue and seriously hurt by a questionable turn in the 11th episode."; // Here goes the second review text.
+
+
+	include 'template_header.php';
+
+?>
