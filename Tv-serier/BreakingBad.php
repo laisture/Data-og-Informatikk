1325e7e12761335c9da2fbf920b2ca81810d8478
Lavanja <lavanja.jeyen@yahoo.no>
2014-11-20 20:49:53 +0100

    Create BreakingBad.php

---
+++ Tv-serier/BreakingBad.php
@@ -0,0 +1,40 @@
+<?php
+
+	$title = "Breaking Bad"; // Here goes the title of the tv-serie.
+
+	$summary = "A high school chemistry teacher turns to a life of crime in order to provide for his family´s future."; // Here goes the summary of the tv-serie.
+
+	$poster ="http://img2.wikia.nocookie.net/__cb20100330051000/breakingbad/images/3/37/Season_3_Poster.jpg"; // Here goes the url for the poster of the tv-serie.
+
+	$altPoster ="The poster of Breaking Bad"; // Here goes the alt text for the poster picture.
+
+	$name1 = "Anita"; // Here goes the name of the the first person who wrote a review.
+
+	$date1 = "Mar 20 2014"; // Here goes the date of the first review.
+
+	$stars1 = "4-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
+
+	$altStars1 = "4 out of 5 stars rating"; // Here goes the alt text for the first stars picture.
+
+	$review1 = "I finally took the plunge to join metacritic because it has been so accurate for me in for so many shows, except Breaking Bad. The cinematography was good, and the story was compelling just barely enough that I forced myself to watch the entire series. Overall, the characters have poor chemistry and do unrealistic things for each other, and the show crawls extremely slow: very soapy dialog and long silent shots of scenery. I don't know who's out there running the conspiracy, but I've seen some great shows and this is not one of them: proceed with caution."; // Here goes the first review text.
+
+	$name2 = "Marvin"; // Here goes the name of the the second person who wrote a review.
+
+	$date2 = "Jul 4 2013"; // Here goes the date of the second review.
+
+	$stars2 = "5-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
+
+	$altStars2 = "5 out of 5 stars rating"; // Here goes the alt text for second the stars picture.
+
+	$review2 = "This is definetly without a shadow of doubt, one of the best series in history, few managed to pull such characters and plot as well as this TV series.Although it may seem that it's slow at first, it's pace is not nearly compared as the quality of story planning, acting and direction.
+In this stage where drama and tension simply take over the audience, Bryan Cranston performs brilliantly only to make people dive in even further into this scenario.It is absolutely amazing how AMC shows us the transformation and the path that all these characters go into, it is now crystal clear how powerful this Vince Gilligan's show can get, and how high can it achieve"; // Here goes the second review text.
+
+
+	include 'template_header.php';
+
+?>
+
+<?php
+
+	include 'template_footer.php';
+?>
