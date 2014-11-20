bcd76db37ec7a8f7e9f07a6d432521c930ab7ec3
Lavanja <lavanja.jeyen@yahoo.no>
2014-11-20 21:01:06 +0100

    Create TheGoodWife.php

---
+++ Tv-serier/TheGoodWife.php
@@ -0,0 +1,39 @@
+<?php
+
+	$title = "The Good Wife"; // Here goes the title of the tv-serie.
+
+	$summary = "Alicia has been a good wife to her husband, a former state´s attorney. After a very humiliating sex and corruption scandal, he is behind bars. She must now provide for her family and returns to work as a litigator in a law firm."; // Here goes the summary of the tv-serie.
+
+	$poster ="http://www.pariscine.com/sites/default/files/affiches_series_tv/the_good_wife_1.jpg"; // Here goes the url for the poster of the tv-serie.
+
+	$altPoster ="The poster of The Good Wife"; // Here goes the alt text for the poster picture.
+
+	$name1 = "Linnea"; // Here goes the name of the the first person who wrote a review.
+
+	$date1 = "Dec 20 2010"; // Here goes the date of the first review.
+
+	$stars1 = "4-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
+
+	$altStars1 = "4 out of 5 stars rating"; // Here goes the alt text for the first stars picture.
+
+	$review1 = "Excellent series, combining courtroom drama, crime stories, politics & social commentaries that are very relevant to today's era, plus romance subplots that are done in mature way. Great cast, and classy storytelling, never trashy. I didn't expect it to be so good in the first place, but ended up being entirely hooked."; // Here goes the first review text.
+
+	$name2 = "Josef"; // Here goes the name of the the second person who wrote a review.
+
+	$date2 = "Sep 1 2011"; // Here goes the date of the second review.
+
+	$stars2 = "4-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
+
+	$altStars2 = "4 out of 5 stars rating"; // Here goes the alt text for second the stars picture.
+
+	$review2 = "I really enjoy the crafty dialog and well developed characters. There are plenty of twists to keep things interesting. My wife and I look forward to watching this show every Sunday!"; // Here goes the second review text.
+
+
+	include 'template_header.php';
+
+?>
+
+<?php
+
+	include 'template_footer.php';
+?>
