3ef757698b8bc2cbf5e2718312f10e9fd5d7d5d8
Lavanja <lavanja.jeyen@yahoo.no>
2014-11-20 20:56:53 +0100

    Create Elementary.php

---
+++ Tv-serier/Elementary.php
@@ -0,0 +1,43 @@
+<?php
+
+	$title = "Elementary"; // Here goes the title of the tv-serie.
+
+	$summary = "A modern take on the cases of Sherlock Holmes, with the detective now living in New York City."; // Here goes the summary of the tv-serie.
+
+	$poster ="http://i.perezhilton.com/wp-content/uploads/2012/06/elementary-poster__oPt.jpg"; // Here goes the url for the poster of the tv-serie.
+
+	$altPoster ="The poster of Elementary"; // Here goes the alt text for the poster picture.
+
+	$name1 = "Elin"; // Here goes the name of the the first person who wrote a review.
+
+	$date1 = "Sep 13 2013"; // Here goes the date of the first review.
+
+	$stars1 = "5-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
+
+	$altStars1 = "5 out of 5 stars rating"; // Here goes the alt text for the first stars picture.
+
+	$review1 = "I was a little worried when I first started watching because it is based off Sherlock Holmes and I didn't want to sit through a show that simply revisits the cases from the books. That being said, I love it. Miller is fantastic as Holmes and Liu is a wonderful lead actress who compliments the Holmes character well as Watson. The stories are enjoyable but a little bit predictable but still entertaining to watch.
+
+I'd recommend it, It's a pretty good show and I don't see how people can hate it let alone give it a negative review."; // Here goes the first review text.
+
+	$name2 = "Edvardsen"; // Here goes the name of the the second person who wrote a review.
+
+	$date2 = "Jan 10 2013"; // Here goes the date of the second review.
+
+	$stars2 = "2-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
+
+	$altStars2 = "2 out of 5 stars rating"; // Here goes the alt text for second the stars picture.
+
+	$review2 = "There is nothing about yhis show that is redeeming. A steaming pile of crap that like a lot of american tv, tries to be successful by copying someone elses ideas. Please stop producing this shit or if you do, please dont export it to australia. Why? Why bother?
+
+This new sherlock is like a sex deprived loser who spouts nonsensical verbal diareah. Whats next? Winnie the pooh reimagined as a crack dealer who bums tigger in a gimp suit.... stop making this crap."; // Here goes the second review text.
+
+
+	include 'template_header.php';
+
+?>
+
+<?php
+
+	include 'template_footer.php';
+?>
