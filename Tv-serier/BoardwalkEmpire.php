ac9519536ae9ad3233440b9ed4d60d72775c5c53
Lavanja <lavanja.jeyen@yahoo.no>
2014-11-20 20:49:13 +0100

    Create BoardwalkEmpire.php

---
+++ Tv-serier/BoardwalkEmpire.php
@@ -0,0 +1,39 @@
+<?php
+
+	$title = "Boardwalk Empire"; // Here goes the title of the tv-serie.
+
+	$summary = "An Atlantic City politician plays both sides of the law, conspiring with gangsters during the Prohibition era."; // Here goes the summary of the tv-serie.
+
+	$poster ="http://ecx.images-amazon.com/images/I/91pPlPbWyiL._SL1500_.jpg"; // Here goes the url for the poster of the tv-serie.
+	
+	$altPoster ="The poster of Boardwalk Empire"; // Here goes the alt text for the poster picture.
+
+	$name1 = "Andreas,"; // Here goes the name of the the first person who wrote a review.
+
+	$date1 = "Dec 3 2010"; // Here goes the date of the first review.
+
+	$stars1 = "2-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
+
+	$altStars1 = "2 out of 5 stars rating"; // Here goes the alt text for the first stars picture.
+
+	$review1 = "So far I am not impressed with the show. It's very boring. The female characters have no personality or backbone. It's supposed to be a time period where women were trying to improve themselves and in this show they appear to be nothing more than sex objects. The gratuitous nudity takes away from the show. It's way too forced. I loved the Sopranos, which had a lot of nudity, but it was never forced like this show does. You don't need to show naked women just to show them. I'm going to give it a little more time to see if it gets better."; // Here goes the first review text.
+
+	$name2 = "Elias"; // Here goes the name of the the second person who wrote a review.
+
+	$date2 = "Apr 2 2012"; // Here goes the date of the second review.
+
+	$stars2 = "5-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
+
+	$altStars2 = "5 out of 5 stars rating"; // Here goes the alt text for second the stars picture.
+
+	$review2 = "First and foremost, the writing on this television program is some of the best I've ever personally seen on the small screen. It's simply prodigious in both quantity and quality. The acting is fantastic. There really isn't a single performance throughout the entire season that I disliked. Perhaps my only criticism of the first twelve episodes is the overall slow, almost snail-like pace in which the story-lines are uncovered. Everything else about the show is great though, the costume & wardrobe departments should hopefully win A LOT of awards!"; // Here goes the second review text.
+
+
+	include 'template_header.php';
+
+?>
+
+<?php
+
+	include 'template_footer.php';
+?>
