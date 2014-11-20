ac97638f0f1bd39a09e8acecc02001d6397b86e3
Lavanja <lavanja.jeyen@yahoo.no>
2014-11-20 20:54:59 +0100

    Create DoctorWho.php

---
+++ Tv-serier/DoctorWho.php
@@ -0,0 +1,39 @@
+<?php
+
+	$title = "Doctor Who"; // Here goes the title of the tv-serie.
+
+	$summary = "A sci-fi tv programme where a lone time lord called the doctor travels in time and space with his companion to save and protect planet Earth for menacing aliens."; // Here goes the summary of the tv-serie.
+
+	$poster ="http://www.panicposters.com/media/catalog/product/cache/1/image/f63dc5ec28f3175f8a7f615bd217eb71/p/p/pp32642-doctor-who-poster.jpg"; // Here goes the url for the poster of the tv-serie.
+
+	$altPoster ="The poster of Doctor Who"; // Here goes the alt text for the poster picture.
+
+	$name1 = "Melissa"; // Here goes the name of the the first person who wrote a review.
+
+	$date1 = "Aug 24 2011"; // Here goes the date of the first review.
+
+	$stars1 = "5-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
+
+	$altStars1 = "5 out of 5 stars rating"; // Here goes the alt text for the first stars picture.
+
+	$review1 = "This is my favorite live-action TV show, period. The first season was great, the second season was fantastic, and it just kept getting better. The various actors that play The Doctor are all fantastic. Christopher Eccleston does a great job in the first season, David Tennant is phenomenal in seasons 2-4, and Matt Smith, although not as good as the first two Doctors, is also great in season 5 (I have yet to see season 6). The Doctor's female companions are also well-cast, from Rose Tyler (played by Billie Piper, my favorite of the companions) to Donna Noble (played by Catherine Tate, who annoyed me at first but my opinion changed as the episodes went on) to Martha Jones (played by Freema Agyeman), to Amy Pond (played by Karen Gillan). It's so exciting; you never know where The Doctor will be next, or what horrors await him on the other side of time. A history of great actors, suspenseful writing, and great special effects. If you are a fan of Science Fiction at all, by any stretch, you owe it to yourself to watch this series. Even if you aren't, give it a go. You won't regret it"; // Here goes the first review text.
+
+	$name2 = "Sondre"; // Here goes the name of the the second person who wrote a review.
+
+	$date2 = "Oct 12 2011"; // Here goes the date of the second review.
+
+	$stars2 = "4-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
+
+	$altStars2 = "4 out of 5 stars rating"; // Here goes the alt text for second the stars picture.
+
+	$review2 = "As an American, British TV was irrelevant to me. That was until I stumbled on this masterpiece. About one year ago I discovered the TV show and soon watched every episode, eventually reaching Season 6, which I loved. If you are a fan of Sci-fi or just very intellectual story-telling, check this show out."; // Here goes the second review text.
+
+
+	include 'template_header.php';
+
+?>
+
+<?php
+
+	include 'template_footer.php';
+?>
