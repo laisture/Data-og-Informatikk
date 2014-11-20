7d36398d9506a3368cb5abc76ee411c54510e66f
Lavanja <lavanja.jeyen@yahoo.no>
2014-11-20 20:54:29 +0100

    Create CougarTown.php

---
+++ Tv-serier/CougarTown.php
@@ -0,0 +1,39 @@
+<?php
+
+	$title = "Cougar Town"; // Here goes the title of the tv-serie.
+
+	$summary = "A recently divorced woman decides to fin some axcitement in her dating life."; // Here goes the summary of the tv-serie.
+
+	$poster ="http://ecx.images-amazon.com/images/I/51%2BV-WpZOoL.jpg"; // Here goes the url for the poster of the tv-serie.
+
+	$altPoster ="The poster of Cougar Town"; // Here goes the alt text for the poster picture.
+
+	$name1 = "Siri"; // Here goes the name of the the first person who wrote a review.
+
+	$date1 = "Jan 7 2010"; // Here goes the date of the first review.
+
+	$stars1 = "3-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
+
+	$altStars1 = "3 out of 5 stars rating"; // Here goes the alt text for the first stars picture.
+
+	$review1 = "This show is so stupid. Just another show about white women obsessing about how they look. This is not cougartown. The whole point is that she is suppose to be dating younger guys and confident that she is older. Now she is dating this new guy Scott Foley and he is just as old as she is. They obsess about eating and that's why white girls are anorexic today. That episode about don't watch me eat was just ridiculous. Courtney Cox is so bony and frail. She just looks sick. They could have chosen someone else for the lead. Her best friend is also a mess. Anyone with eyes can tell that she injects her face with botox. This whole show is silly but mess like this will stay on the tv for years."; // Here goes the first review text.
+
+	$name2 = "Karin"; // Here goes the name of the the second person who wrote a review.
+
+	$date2 = "Oct 7 2009"; // Here goes the date of the second review.
+
+	$stars2 = "2-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
+
+	$altStars2 = "2 out of 5 stars rating"; // Here goes the alt text for second the stars picture.
+
+	$review2 = "This show is simply to painful to take seriously. A 40 year old woman starving to satisfy her sexual desires with 20 year olds, while at the same time being a mom isn't a nice plot line. I really hope this gets cancelled soon...for Courtney Cox's sake. She really does herself a disservice in this series"; // Here goes the second review text.
+
+
+	include 'template_header.php';
+
+?>
+
+<?php
+
+	include 'template_footer.php';
+?>
