2338f9581ee5bafc908b41a4f17a36e5aef0acf7
Lavanja <lavanja.jeyen@yahoo.no>
2014-11-20 20:55:45 +0100

    Create Dexter.php

---
+++ Tv-serier/Dexter.php
@@ -0,0 +1,39 @@
+<?php
+
+	$title = "Dexter"; // Here goes the title of the tv-serie.
+
+	$summary = "A Miami police forensics expert moonlights as a serial killer of criminals whom he believes have escaped justice."; // Here goes the summary of the tv-serie.
+
+	$poster ="http://pmctvline2.files.wordpress.com/2011/08/dexter_firstlook_600110812071729.jpg"; // Here goes the url for the poster of the tv-serie.
+
+	$altPoster ="The poster of Dexter"; // Here goes the alt text for the poster picture.
+
+	$name1 = "David"; // Here goes the name of the the first person who wrote a review.
+
+	$date1 = "Dec 17 2008"; // Here goes the date of the first review.
+
+	$stars1 = "5-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
+
+	$altStars1 = "5 out of 5 stars rating"; // Here goes the alt text for the first stars picture.
+
+	$review1 = "Best...Show...Ever! After the first five minutes into Season One, I've been hooked. I've even got my dad addicted to this. I'm venturing to say this is better than The Simpsons."; // Here goes the first review text.
+
+	$name2 = "Mona Lisa"; // Here goes the name of the the second person who wrote a review.
+
+	$date2 = "Oct 7 2006"; // Here goes the date of the second review.
+
+	$stars2 = "2-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
+
+	$altStars2 = "2 out of 5 stars rating"; // Here goes the alt text for second the stars picture.
+
+	$review2 = "Dexter's mind is the fancy of the modern world and the writers morbid sense is displayed as a virtuoso.Murder and enjoyment of such things as entertainment is the highest example of America in decline. To enjoy perversity is to lose all human inhibitions. Watch Dexter and you will."; // Here goes the second review text.
+
+
+	include 'template_header.php';
+
+?>
+
+<?php
+
+	include 'template_footer.php';
+?>
