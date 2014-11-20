8fba69f1b0a3f5c46a8e0c64ff185aa3961995e1
Sara-K <sarakhan@Sara-Khans-MacBook-Pro.local>
2014-11-20 21:14:58 +0100

    Tv-serier

---
+++ Tv-serier/Lost.php
@@ -0,0 +1,41 @@
+<?php
+
+	$title = "Lost"; // Here goes the title of the tv-serie.
+
+	$summary = "The survivors of a plane crash are forced to live with each other on a remote island, a dangerous new world that poses unique threats of its own."; // Here goes the summary of the tv-serie.
+
+	$poster ="http://img2.wikia.nocookie.net/__cb20091118030241/lostpedia/images/9/94/LostSeason6OfficialPoster.jpg"; // Here goes the url for the poster of the tv-serie.
+
+	$altPoster ="The poster of Lost"; // Here goes the alt text for the poster picture.
+
+	$name1 = "Dominique,"; // Here goes the name of the the first person who wrote a review.
+
+	$date1 = "16 Nov 2014"; // Here goes the date of the first review.
+
+	$stars1 = "5-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
+
+	$altStars1 = "5 out of 5 stars rating"; // Here goes the alt text for the first stars picture.
+
+	$review1 = "Didn't even know too much about this show until my mom was watching the season 2 premiere! I eventually went back and watched season 1. I loved this show instantly! You definetly have to watch it from the beginning or you will be very confused. Season 1 usually focused on one character per episode so you can get a quick background on where they came from and why they are on the island which is important to know! There were a lot of characters on this show but you will love them! the acting is fantastic! My favorite character was Charlie by the way.
+After each episode, you always wanted to know more! it was so addicting! Definetly one of the greatest shows i ever watched! Highly recommended!"; // Here goes the first review text.
+
+	$name2 = "Bobbie,"; // Here goes the name of the the second person who wrote a review.
+
+	$date2 = "14 Aug 2014"; // Here goes the date of the second review.
+
+	$stars2 = "5-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
+
+	$altStars2 = "5 out of 5 stars rating"; // Here goes the alt text for second the stars picture.
+
+	$review2 = "A show that can offer question after question with very few answers and still remain utterly engrossing is something special. The show has a huge cast, expanding as the show progresses, all of whom are very interesting due to the characters all being fleshed out through flashbacks. Mystery, intrigue, suspense, action, drama. The amalgamation of these ingredients make for one of the best shows ever made.
+The enjoyment of the show comes from the journey not the ending, and what a journey it is."; // Here goes the second review text.
+
+
+	include 'template_header.php';
+
+?>
+
+<?php
+
+	include 'template_footer.php';
+?>
