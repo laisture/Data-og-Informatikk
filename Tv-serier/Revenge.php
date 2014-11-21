3d981c5f7964cd64b97567dfe7509294d0ce1055
Sara-K <sarakhan@Sara-Khans-MacBook-Pro.local>
2014-11-21 13:17:33 +0100

    .

---
+++ Tv-serier/Revenge.php
@@ -0,0 +1,39 @@
+<?php
+
+	$title = "Revenge"; // Here goes the title of the tv-serie.
+
+	$summary = "The daughter of a framed executive makes it her mission to infiltrate upper Hamptons society and exact revenge on the people who set her father up.	"; // Here goes the summary of the tv-serie.
+
+	$poster ="http://2.bp.blogspot.com/-c8uk27KLdfk/UCb1AaVpe9I/AAAAAAAADP0/THzCFi3qezM/s1600/revenge-season-2-poster.jpg"; // Here goes the url for the poster of the tv-serie.
+
+	$altPoster ="The poster of Revenge"; // Here goes the alt text for the poster picture.
+
+	$name1 = "An,"; // Here goes the name of the the first person who wrote a review.
+
+	$date1 = "09 Sept 2014"; // Here goes the date of the first review.
+
+	$stars1 = "5-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
+
+	$altStars1 = "5 out of 5 stars rating"; // Here goes the alt text for the first stars picture.
+
+	$review1 = "The best new show! Emily VanCamp does an AMAZING job playing Emily Thorne (Amanda Clarke). She can make little micro-expressions in her face which, lets face it, not many actors and actresses can do. Each episode gets you more drawn into the series' characters and wanting to know what happens next. The show is really well written and the entire cast is really talented."; // Here goes the first review text.
+
+	$name2 = "Kim,"; // Here goes the name of the the second person who wrote a review.
+
+	$date2 = "14 Jun 2014"; // Here goes the date of the second review.
+
+	$stars2 = "3-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
+
+	$altStars2 = "3 out of 5 stars rating"; // Here goes the alt text for second the stars picture.
+
+	$review2 = "So far this show seems promising. The dialogue may be a bit corny and forced at times, but the actors have portrayed their characters very well. The scenery and the directing is absolutely gorgeous. If the story can develop well and be interesting, I think this show will be one heck of a ride."; // Here goes the second review text.
+
+
+	include 'template_header.php';
+
+?>
+
+<?php
+
+	include 'template_footer.php';
+?>
