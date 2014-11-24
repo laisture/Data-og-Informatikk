d852ffbbc3a3724c76cf1f93b76fd1f4cdb067c8
Sara-K <sarakhan@Sara-Khans-MacBook-Pro.local>
2014-11-24 13:25:46 +0100

    -

--- PHP Template/Template_footer.php
+++
@@ -1,4 +0,0 @@
-<!--Here comes the layout of the page-->
-	</body>
-	<footer><!--Here comes footer of the page--></footer>
-</html>
--- PHP Template/Template_header.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE HTML>
-<html>
-	<head>
-		<link type="text/css" rel="stylesheet" href="stylesheet.css"/>
-		<title><!--Here comes the tittle of the page--></title>
-	</head>
-	<body>
-
-	<!--Here comes the layout of the page-->
