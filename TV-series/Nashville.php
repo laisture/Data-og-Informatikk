cf9e4ee20e3f7b207aaae02b536474d123e7e694
Lavanja <lavanja.jeyen@yahoo.no>
2014-11-29 12:50:19 +0100

    Update Nashville.php

--- TV-series/Nashville.php
+++ TV-series/Nashville.php
@@ -2,7 +2,7 @@
 
 	$title = "Nashville"; // Here goes the title of the tv-serie.
 
-	$summary = "A fading Nashville superstar is forced to team up with a teen sensation or face the loss of her tour and promotion of her latest records.	"; // Here goes the summary of the tv-serie.
+	$summary = "A Nashville superstar is forced to be in a team with a teen sensation. If he don’t he must face the loss of her tour an promotion. "; // Here goes the summary of the tv-serie.
 
 	$poster ="../Pictures/Nashville.jpg"; // Here goes the url for the poster of the tv-serie.
 
@@ -16,7 +16,7 @@
 
 	$altStars1 = "4 out of 5 stars rating"; // Here goes the alt text for the first stars picture.
 
-	$review1 = "This show is getting better every episode, and i am going to be extremely disappointed if it does get cancelled, I Truly hope they renew this series for another season it is one of the best tv drama's I have seen in quite some time and I truly enjoy watching it.. usually I watch supernatural style shows but this one entertains me on another level that other shows do not seem to deliver, maybe it is because I am from the Nashville area and know people in the music business but it is an extremely great show and would be a shame to see it go away."; // Here goes the first review text.
+	$review1 = "This series is getting better and better by every episode. I truly enjoy watching it. I usually don’t like watching series, but this series is something special. It is something that makes this different from all of the other series. I really love it. "; // Here goes the first review text.
 
 	$name2 = "Shanta,"; // Here goes the name of the the second person who wrote a review.
 
@@ -26,8 +26,7 @@
 
 	$altStars2 = "5 out of 5 stars rating"; // Here goes the alt text for second the stars picture.
 
-	$review2 = "Nashville is one of my favorite shows on television! For lovers of all kinds of music, Nashville is a must see!
-For anyone else who is wondering, the actor who plays Liam is probably not going to be seen regularly on Nashville because he has taken on the role of Daario Naharis on season 4 of GOT. I don't particularly care for him in that role, but I love him as Liam. Still he's doing well for himself if he landed on a show as critically acclaimed as GOT :)"; // Here goes the second review text.
+	$review2 = "What I really love about this series is that you never really get tired of it. The path and transformation that the characters go into is shown absolutely amazing. This show has one of the best story lines in all of TV. I have never seen a poorly written or produced episode at all. The series is original and in my opinion it is flawless. The amazing casting makes this series a success and I will definitely keep watching this show for as long as it stays on. "; // Here goes the second review text.
 
 
 	include 'template_header.php';
