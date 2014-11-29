d3e62f83b334eb0ec288ecdc67a30a8d49eb5e03
Lavanja <lavanja.jeyen@yahoo.no>
2014-11-29 14:06:15 +0100

    Update DaysOfOurLives.php

--- TV-series/DaysOfOurLives.php
+++ TV-series/DaysOfOurLives.php
@@ -2,7 +2,7 @@
 
 	$title = "Days Of Our Lives"; // Here goes the title of the tv-serie.
 
-	$summary = "A chronicle of the lives, loves, trials and tribulations of the citizens of the fictional city of Salem."; // Here goes the summary of the tv-serie.
+	$summary = "A chronicle of the lives, loves, trials and tribulations of the citizens."; // Here goes the summary of the tv-serie.
 
 	$poster ="../Pictures/DaysOfOurLives.jpg"; // Here goes the url for the poster of the tv-serie.
 
@@ -16,7 +16,7 @@
 
 	$altStars1 = "3 out of 5 stars rating"; // Here goes the alt text for the first stars picture.
 
-	$review1 = "I have been watching Days for 50 years!! Yes. My Grandma and I watched it when I was a kid. I've seen many characters come and go. This has to be the worst cast ever. New people every week. Older characters dumped. It's very sad to me. I really wish that Daniel would get together with Jen! I find the whole show frustrating. I realize that you need younger viewers, but what thetlpeople who have been watching forever? I work full-time, so I usually have to catch up. I knew Sammy was leaving, but E J? Then old Dimera goes on and done. I'll miss my old Suzanne;" // Here goes the first review text.
+	$review1 = "After only watching the first episode, I can tell it is a mindless and predictable series. When the first episodes began, I was interested and curious to find out what would happen, but after a while I got disappointed. The first episodes is well written and nicely produced but it feels empty and plot driven. But if you really enjoy this genre you may want to give this series a chance." // Here goes the first review text.
 
 	$name2 = "Leo"; // Here goes the name of the the second person who wrote a review.
 
@@ -26,7 +26,7 @@
 
 	$altStars2 = "3 out of 5 stars rating"; // Here goes the alt text for second the stars picture.
 
-	$review2 = "I am so unhappy with the days of our lives writers. What happened to John and it showing it in what the broad did and then he said, she was innocent that it was him, being in a rage. You guys really suck. You don't want to write the idiot woman out of the show. I have watched this show for 15 years and I am finished. If you can't write any better than this then I am finished watching this show."; // Here goes the second review text.
+	$review2 = "After only watching the first episode, I can tell it is a mindless and predictable series. The first episodes is well written and nicely produced but it feels empty and plot driven. But if you really enjoy this genre you may want to give this series a chance."; // Here goes the second review text.
 
 
 	include 'template_header.php';
