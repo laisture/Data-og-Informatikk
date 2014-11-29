3e9ea8d6d6f334dd3f4a561e7d585c8b7733d10f
Sara-K <sarakhan.95@hotmail.com>
2014-11-29 14:11:43 +0100

    Update Grimm.php

--- TV-series/Grimm.php
+++ TV-series/Grimm.php
@@ -2,7 +2,7 @@
 
 	$title = "Grimm"; // Here goes the title of the tv-serie.
 
-	$summary = "A homicide detective discovers he is a descendant of hunters who fight supernatural forces."; // Here goes the summary of the tv-serie.
+	$summary = "Nick Burkhardt is a homicide detective who discovers that he is a descendant of hunters who fight supernatural forces."; // Here goes the summary of the tv-serie.
 
 	$poster ="../Pictures/Grimm.jpg"; // Here goes the url for the poster of the tv-serie.
 
@@ -16,17 +16,17 @@
 
 	$altStars1 = "3 out of 5 stars rating"; // Here goes the alt text for the first stars picture.
 
-	$review1 = "Excellent series, combining courtroom drama, crime stories, politics & social commentaries that are very relevant to today's era, plus romance subplots that are done in mature way. Great cast, and classy storytelling, never trashy. I didn't expect it to be so good in the first place, but ended up being entirely hooked."; // Here goes the first review text.
+	$review1 = "After only watching the first episode, I can tell it is a mindless and predictable series. When the first episodes began, I was interested and curious to find out what would happen, but after a while I got disappointed. The first episodes is well written and nicely produced but it feels empty and plot driven. But if you really enjoy this genre you may want to give this series a chance."; // Here goes the first review text.
 
 	$name2 = "Daniel"; // Here goes the name of the the second person who wrote a review.
 
 	$date2 = "Jun 12 2012"; // Here goes the date of the second review.
 
-	$stars2 = "3-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
+	$stars2 = "4-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
 
-	$altStars2 = "3 out of 5 stars rating"; // Here goes the alt text for second the stars picture.
+	$altStars2 = "4 out of 5 stars rating"; // Here goes the alt text for second the stars picture.
 
-	$review2 = "The legal plots are all Been there, seen that and therefore are rather boring. It's a shame that interesting characters are not given much interesting material. Scenes here and there are outstanding, but if this series is just going to be case of the week on a regular basis, what a terrible shame."; // Here goes the second review text.
+	$review2 = "This series combines a strong cast with good writing. Every episode is getting better as the series go on and I definitely think the latest episodes are better than the previous ones. The actor/actresses are superb and they will grow on you eventually."; // Here goes the second review text.
 
 
 	include 'template_header.php';
