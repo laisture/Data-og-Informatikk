ba0cbd45f66bbb1ec5ae8ba8a8416f492e9af046
Sara-K <sarakhan.95@hotmail.com>
2014-11-29 14:12:58 +0100

    Update Fargo.php

--- TV-series/Fargo.php
+++ TV-series/Fargo.php
@@ -2,7 +2,7 @@
 
 	$title = "Fargo"; // Here goes the title of the tv-serie.
 
-	$summary = "A drifter named Lorne Malvo arrives in small-town Minnesota and influences the population with his malice and violence, including put-upon insurance salesman Lester Nygaard."; // Here goes the summary of the tv-serie.
+	$summary = "Jerry Lundegard is in an unknown financial jam and the only way to solve it is with his father-in-law's money. His plan for getting the money involves kidnapping, criminals and killings."; // Here goes the summary of the tv-serie.
 
 	$poster ="../Pictures/Fargo.jpg"; // Here goes the url for the poster of the tv-serie.
 
@@ -16,7 +16,7 @@
 
 	$altStars1 = "5 out of 5 stars rating"; // Here goes the alt text for the first stars picture.
 
-	$review1 = "Wow. After Breaking Bad ended, I was looking for a great new show to fill that void, but I was doubtful I could find one. Fargo has filled that void. The amazing casting alone makes this show a success. A lot of the mainstream networks (NBC, ABC, CBS, TNT) fill their shows with actors who are bland model types - people who are just boring to watch. It's refreshing to find a show that casts interesting actors who aren't just cookie-cutter Ken and Barbie clones. The writing is great, but the interesting and likable actors really take this show to another level. It's great when you can tell the casting director wasn't trying to just fill all the roles with hot people, but rather trying to find people who were charismatic and believable in their roles."; // Here goes the first review text.
+	$review1 = "I was looking for a great new show and this is it! What I really love about this series is that you never really get tired of it. The path and transformation that the characters go into is shown absolutely amazing. This show has one of the best story lines in all of TV. I have never seen a poorly written or produced episode at all. The series is original and in my opinion it is flawless. The amazing casting makes this series a success and I will definitely keep watching this show for as long as it stays on. "; // Here goes the first review text.
 
 	$name2 = "Jones"; // Here goes the name of the the second person who wrote a review.
 
@@ -26,7 +26,7 @@
 
 	$altStars2 = "2 out of 5 stars rating"; // Here goes the alt text for second the stars picture.
 
-	$review2 = "Tiresome, slow and not at all exciting. What a waste of time. Will not be waiting for Season 2! Probably one of the worst I've ever watched- for all it's good reviews. Oh well!"; // Here goes the second review text.
+	$review2 = "OMG! This series is very very bad. I didn’t like the story or the characters. This is not interesting, they don´t even know how to act. It´s so painful. Anyone else could have done this. It´s so many talents out there. Find them instead of using characters like this. I have just wasted my time by watching this bad series. I couldn´t watch more than a few episodes, but that was enough. Who would waste their time watching this?"; // Here goes the second review text.
 
 
 	include 'template_header.php';
