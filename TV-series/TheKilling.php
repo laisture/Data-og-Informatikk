a60d7133fad1178dd572836b4440260aa1e62f9f
Lavanja <lavanja.jeyen@yahoo.no>
2014-11-29 12:44:01 +0100

    Update TheKilling.php

--- TV-series/TheKilling.php
+++ TV-series/TheKilling.php
@@ -2,7 +2,7 @@
 
 	$title = "The Killing"; // Here goes the title of the tv-serie.
 
-	$summary = "A police investigation, the saga of a grieving family, and a Seattle mayoral campaign all interlock after the body of 17-year-old Rosie Larsen is found in the trunk of a submerged car.	"; // Here goes the summary of the tv-serie.
+	$summary = "A police investigation after the body of 17-years old Rosie Larsen is found in the trunk of a submerged car. "; // Here goes the summary of the tv-serie.
 
 	$poster ="../Pictures/TheKilling.jpg"; // Here goes the url for the poster of the tv-serie.
 
@@ -16,7 +16,7 @@
 
 	$altStars1 = "5 out of 5 stars rating"; // Here goes the alt text for the first stars picture.
 
-	$review1 = "The Killing has become my absolute favorite show of all time. I cannot get over its depth and precision. I'm in awe of this series and am grateful for the wonderfully talented actors who bring it to life. I just recently discovered the show a few days before the season two premiere, and I watched the first season in two days. I wish I could watch season two in this way -- waiting between episodes is aggravating to say the very least. Bravo to all who are involved in this series, and I cannot wait for more!"; // Here goes the first review text.
+	$review1 = "Something like The Killing has become my favorite show of all time. I have watched the entire series and could not wait for each upcoming episode. I have no words to say, I just love it. This is just so good! "; // Here goes the first review text.
 
 	$name2 = "Edmund,"; // Here goes the name of the the second person who wrote a review.
 
@@ -26,7 +26,7 @@
 
 	$altStars2 = "5 out of 5 stars rating"; // Here goes the alt text for second the stars picture.
 
-	$review2 = "I watched the entire series and could not wait for each upcoming episode. And the season finale was just the best icing on any cake. Way to finish it. I can't wait for it's return. Yes there is a lot of raining. Yes it's very dark. Yes it's not C.S.I. and it's paced very slowly. It's more like Mystic River in it's plotting. It's extremely depressing at moments. But it's emotionally thorough. It's smart too. The police work seems to me what an exhausting mysterious case would be for a dedicated detective. And I felt compassion for her trying to be a parent and be a dedicated detective. I can understand where people are not interested. It's not just quick resolutions every Night. It's an emotional roller coaster and at times gut-wrenching and challenging. It's everything I would choose over Hawaii-50, C.S.I. or any standard television. I'll be back for sure."; // Here goes the second review text.
+	$review2 = "What I really love about this series is that you never really get tired of it. The path and transformation that the characters go into is shown absolutely amazing. This show has one of the best story lines in all of TV. I have never seen a poorly written or produced episode at all. The series is original and in my opinion it is flawless. The amazing casting makes this series a success and I will definitely keep watching this show for as long as it stays on."; // Here goes the second review text.
 
 
 	include 'template_header.php';
