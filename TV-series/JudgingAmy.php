fff0f94886c5bc0dc93e6ddbb1f2bcd13718e73d
Lavanja <lavanja.jeyen@yahoo.no>
2014-11-29 14:39:43 +0100

    Update JudgingAmy.php

--- TV-series/JudgingAmy.php
+++ TV-series/JudgingAmy.php
@@ -2,7 +2,7 @@
 
 	$title = "Judging Amy"; // Here goes the title of the tv-serie.
 
-	$summary = "A family drama focused on three generations of women living together in Hartford, Connecticut. Amy Brenneman plays Amy Gray, who left New York City behind and now works as a family court judge. Karle Warren plays Lauren, Amy's daughter; Tyne Daly is Maxine, Amy's tough, opinionated mother."; // Here goes the summary of the tv-serie.
+	$summary = "A family drama focused on three generations of women living together."; // Here goes the summary of the tv-serie.
 
 	$poster ="../Pictures/JudgingAmy.jpg"; // Here goes the url for the poster of the tv-serie.
 
@@ -16,17 +16,17 @@
 
 	$altStars1 = "5 out of 5 stars rating"; // Here goes the alt text for the first stars picture.
 
-	$review1 = "There are hardly words to say about this show it is one that you can watch over and over again and never get tired of it. You can watch it and it feels like real life ot is not fake and totally out there and leave you thinking that could and would never happen not this show. Thi show is completely real it shows a family that has there struggles and there problems and how they get through them although they might no always turn out good it is great. It has some happy episodes and some tearjerkers but it always brought me back for more and i would love it if this show was still on because every time that i watch reruns it still brings me back for more."; // Here goes the first review text.
+	$review1 = "What I really love about this series is that you never really get tired of it. The path and transformation that the characters go into is shown absolutely amazing. This show has one of the best story lines in all of TV. I have never seen a poorly written or produced episode at all. The series is original and in my opinion it is flawless. The amazing casting makes this series a success and I will definitely keep watching this show for as long as it stays on. "; // Here goes the first review text.
 
 	$name2 = "May,"; // Here goes the name of the the second person who wrote a review.
 
 	$date2 = "14 Aug 2014"; // Here goes the date of the second review.
 
-	$stars2 = "5-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
+	$stars2 = "2-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
 
-	$altStars2 = "5 out of 5 stars rating"; // Here goes the alt text for second the stars picture.
+	$altStars2 = "2 out of 5 stars rating"; // Here goes the alt text for second the stars picture.
 
-	$review2 = "Judging Amy is a TV show which certainly gets you hooked once you watch it and it also shows improving topics on each and every episode. All of the actors/actresses of the show are very excellent in playing their roles. I was sad when it got cancelled. I missed watching this series in the line of primetime shows and glad to know that I found a copy and purchased one at memorylanedvd .com . I received an absolutely amazing set! Complete episodes with great quality. Very satisfied with the order and definitely thankful to watch again a well-produced TV series of all time!"; // Here goes the second review text.
+	$review2 = "OMG! This series is very very bad. I didnt like the story or the characters. This is not interesting, they don´t even know how to act.  It´s so painful. Anyone else could have done this. It´s soo many talents out there. Find them instead of using characters like this. Have just wasted my time by watching this bad series. I couldn´t watch more than a few episodes, but that was enough. Who would waste your time watching this?"; // Here goes the second review text.
 
 
 	include 'template_header.php';
