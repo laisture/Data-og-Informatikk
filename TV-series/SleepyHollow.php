bf4da4ec27f6ad2a8bb9b6f107a400f58d7270d3
Sara-K <sarakhan.95@hotmail.com>
2014-11-29 13:33:40 +0100

    Update SleepyHollow.php

--- TV-series/SleepyHollow.php
+++ TV-series/SleepyHollow.php
@@ -2,7 +2,7 @@
 
 	$title = "Sleepy Hollow"; // Here goes the title of the tv-serie.
 
-	$summary = "Ichabod Crane is resurrected and pulled two and a half centuries through time to unravel a mystery that dates all the way back to the founding fathers.	"; // Here goes the summary of the tv-serie.
+	$summary = "Ichabod Crane is resurrected and pairs up with a present-day cop. Together they save the town of Sleepy Hollow from evil. Clues from past mysteries enlighten the present as each episode features a flashback to Ichabods's 1776 life to unravel a mystery that dates all the way back to the founding fathers."; // Here goes the summary of the tv-serie.
 
 	$poster ="../Pictures/SleepyHollow.jpg"; // Here goes the url for the poster of the tv-serie.
 
@@ -16,17 +16,17 @@
 
 	$altStars1 = "4 out of 5 stars rating"; // Here goes the alt text for the first stars picture.
 
-	$review1 = "Lots to like here if this is your genre. Agree with what everyone else is saying fast -paced, good acting, nice cinematography. The over arching plot is interesting and has loads of potential to expand, though it does seem like a show where you have to watch to keep up with the mythology. There were a few clichéd, annoying plot elements early in the pilot but thankfully they cleaned them up and moved on before the hour was up. And one super minor blooper, the real village of Sleepy Hollow has a population of 9870. For the series, they used the Village of Sleepy Hollow sign but changed the pop. to 144,000, which is a city, not a village."; // Here goes the first review text.
+	$review1 = "Lots to like here if this is your genre. Good acting! There were some clichéd, annoying plot elements early in the pilot. But it got better and now it is one of my favourite series. The amazing casting makes this series a success and I will definitely keep watching this show for as long as it stays on."; // Here goes the first review text.
 
 	$name2 = "Fred,"; // Here goes the name of the the second person who wrote a review.
 
 	$date2 = "05 Aug 2014"; // Here goes the date of the second review.
 
-	$stars2 = "4-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
+	$stars2 = "5-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
 
-	$altStars2 = "4 out of 5 stars rating"; // Here goes the alt text for second the stars picture.
+	$altStars2 = "5 out of 5 stars rating"; // Here goes the alt text for second the stars picture.
 
-	$review2 = "Can't wait for each episode. Was initially bummed because I thought each episode would only center on the deadly deeds of the horseman....but the trajectory of the series changed with the second episode. Love the vocal banter of the two leads, thankful for the metered way in which the backstory is revealed, and am already trying to figure out which characters in the police precinct are aware of what is going on in Sleepy Hollow. Thanks for not dragging out the storyline....which is why I stopped watching Once Upon A Time, Grimm (you've seen one, you've seen them all), and Lost. Good show FOX. I'm hooked."; // Here goes the second review text.
+	$review2 = "Can't wait for each episode, I got hooked at once. This series combines a strong cast with good writing. Every episode is getting better as the series go on and I definitely think the latest episodes are better than the previous ones. The actor/actresses are superb and they will grow on you eventually."; // Here goes the second review text.
 
 
 	include 'template_header.php';
