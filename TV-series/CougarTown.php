88b1960c0b07ae9d017b8e5f13c516e33da3ad34
Sara-K <sarakhan@Sara-Khans-MacBook-Pro.local>
2014-11-29 12:39:20 +0100

    -

--- TV-series/CougarTown.php
+++ TV-series/CougarTown.php
@@ -2,7 +2,7 @@
 
 	$title = "Cougar Town"; // Here goes the title of the tv-serie.
 
-	$summary = "A recently divorced woman decides to fin some axcitement in her dating life."; // Here goes the summary of the tv-serie.
+	$summary = "Jules Cobb is a recently divorced mom. After spending her 20's raising her son she decides that she wants to find some excitement in her dating life."; // Here goes the summary of the tv-serie.
 
 	$poster ="../Pictures/CougarTown.jpg"; // Here goes the url for the poster of the tv-serie.
 
@@ -10,23 +10,23 @@
 
 	$name1 = "Siri"; // Here goes the name of the the first person who wrote a review.
 
-	$date1 = "Jan 7 2010"; // Here goes the date of the first review.
+	$date1 = "Jan 7 2014"; // Here goes the date of the first review.
 
-	$stars1 = "3-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
+	$stars1 = "2-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
 
-	$altStars1 = "3 out of 5 stars rating"; // Here goes the alt text for the first stars picture.
+	$altStars1 = "2 out of 5 stars rating"; // Here goes the alt text for the first stars picture.
 
-	$review1 = "This show is so stupid. Just another show about white women obsessing about how they look. This is not cougartown. The whole point is that she is suppose to be dating younger guys and confident that she is older. Now she is dating this new guy Scott Foley and he is just as old as she is. They obsess about eating and that's why white girls are anorexic today. That episode about don't watch me eat was just ridiculous. Courtney Cox is so bony and frail. She just looks sick. They could have chosen someone else for the lead. Her best friend is also a mess. Anyone with eyes can tell that she injects her face with botox. This whole show is silly but mess like this will stay on the tv for years."; // Here goes the first review text.
+	$review1 = "When the first episodes began, I was interested and curious to find out what would happen, but after a while I got disappointed. This show is stupid! The first episodes is OK but it feels empty and plot driven. This whole show is silly but mess like this will stay on the tv for years."; // Here goes the first review text.
 
 	$name2 = "Karin"; // Here goes the name of the the second person who wrote a review.
 
-	$date2 = "Oct 7 2009"; // Here goes the date of the second review.
+	$date2 = "Oct 7 2014"; // Here goes the date of the second review.
 
-	$stars2 = "2-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
+	$stars2 = "4-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
 
-	$altStars2 = "2 out of 5 stars rating"; // Here goes the alt text for second the stars picture.
+	$altStars2 = "4 out of 5 stars rating"; // Here goes the alt text for second the stars picture.
 
-	$review2 = "This show is simply to painful to take seriously. A 40 year old woman starving to satisfy her sexual desires with 20 year olds, while at the same time being a mom isn't a nice plot line. I really hope this gets cancelled soon...for Courtney Cox's sake. She really does herself a disservice in this series"; // Here goes the second review text.
+	$review2 = "This series combines a strong cast with good writing. It's funny, smart and I love the characters. Every episode is getting better as the series go on and I definitely think the latest episodes are better than the previous ones. "; // Here goes the second review text.
 
 
 	include 'template_header.php';
