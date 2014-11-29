f216d898db1b907682ffdf019a658b51e98f707e
Lavanja <lavanja.jeyen@yahoo.no>
2014-11-29 12:47:00 +0100

    Update Helix.php

--- TV-series/Helix.php
+++ TV-series/Helix.php
@@ -2,7 +2,7 @@
 
 	$title = "Helix"; // Here goes the title of the tv-serie.
 
-	$summary = "A team of scientists are thurst into a potentially life-or-death situation in this thriller, which begins with the group being deployed to the Arctic to secretly investigate what could be a disease outbreak."; // Here goes the summary of the tv-serie.
+	$summary = "It is about a team of scientists who are thurst into a potentially life or death situation. It start with the group being deployed to secretly investigate."; // Here goes the summary of the tv-serie.
 
 	$poster ="../Pictures/Helix.jpg"; // Here goes the url for the poster of the tv-serie.
 
@@ -16,7 +16,7 @@
 
 	$altStars1 = "3 out of 5 stars rating"; // Here goes the alt text for the first stars picture.
 
-	$review1 = "I have watched only the pilot so far and I am in the process of deciding if I will invest any more time in Helix. So far I find it to be a major disappointment. Start off with ´The Thing´, add some mad-scientist experiment elements, chase scenes in the ventilation ducts, a little of this, a little of that. Have I left anything out? Oh, yes. Imagination, quality writing, skillful acting (with a few exceptions), real suspense."; // Here goes the first review text.
+	$review1 = "This series is just waste of time. Its getting boring, and the same things are happen again and again. I was so interested to watch it when the first episode was out, but I got disappointed. But if you like this kind of genre, please give it a chance. "; // Here goes the first review text.
 
 	$name2 = "Eva"; // Here goes the name of the the second person who wrote a review.
 
@@ -26,7 +26,7 @@
 
 	$altStars2 = "4 out of 5 stars rating"; // Here goes the alt text for second the stars picture.
 
-	$review2 = "4 episodes in, this Syfy drama has shown its willingness to create mystery. It's utilized the horror associated with the virus to explore character histories and the mythology of Helix's world in general. The biggest pitfalls result from the archetypal characters. But a few of these characters are already being fleshed out. That said, it's been fun trying to unpack the source of the virus and the duplicitous intentions of several of the characters."; // Here goes the second review text.
+	$review2 = "This series combines a strong cast with good writing. Every episode is getting better as the series go on and I definitely think the latest episodes are better than the previous ones. The actor/actresses are superb and they will grow on you eventually."; // Here goes the second review text.
 
 
 	include 'template_header.php';
