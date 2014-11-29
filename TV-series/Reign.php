3d954904f1cd42e0ad312ba3fdde161f5a7e5271
Sara-K <sarakhan.95@hotmail.com>
2014-11-29 15:12:37 +0100

    Update Reign.php

--- TV-series/Reign.php
+++ TV-series/Reign.php
@@ -2,7 +2,7 @@
 
 	$title = "Reign"; // Here goes the title of the tv-serie.
 
-	$summary = "Chronicles the rise to power of Mary Queen of Scots when she arrives in France as a 15-years-old, betrothed to Prince Francis, and with her four best friends as ladies-in-waiting."; // Here goes the summary of the tv-serie.
+	$summary = "Mary Queen of Scots awaits her marrige to Prince Francis, to whom she has been betrothed to at age six."; // Here goes the summary of the tv-serie.
 
 	$poster ="../Pictures/Reign.jpg"; // Here goes the url for the poster of the tv-serie.
 
@@ -16,18 +16,17 @@
 
 	$altStars1 = "4 out of 5 stars rating"; // Here goes the alt text for the first stars picture.
 
-	$review1 = "I love the show , but having the king acting like a loony tunes does nothing positive for the show.
-So, it's a little disappointing in that area. I feel there is a lack of action and suspense should be upped a notch. Otherwise the show is entertaining. Yes, I will continue to watch in the hopes it gets better."; // Here goes the first review text.
+	$review1 = "This series combines a strong cast with good writing. Every episode is getting better as the series go on and I definitely think the latest episodes are better than the previous ones. The actor/actresses are superb and they will grow on you eventually."; // Here goes the first review text.
 
 	$name2 = "Ole-Hansen"; // Here goes the name of the the second person who wrote a review.
 
-	$date2 = "Oct 25 2013"; // Here goes the date of the second review.
+	$date2 = "Oct 25 2014"; // Here goes the date of the second review.
 
 	$stars2 = "5-stars.png"; // Here goes the image url for how many stars the user has given the tv-serie.
 
 	$altStars2 = "5 out of 5 stars rating"; // Here goes the alt text for second the stars picture.
 
-	$review2 = "Best show on the CW and best new show of the fall. It's getting overlooked because it is pretty much the only thing on the CW that is not based on popular YA books and doesn't have vampires. REIGN is mysterious, thrilling, dramatic, romantic and dark, and it should be at the top of your DVR. The cast is pretty spectacular as well, especially Adalaide Kane as Mary and Megan Follows as Catherine de Medici."; // Here goes the second review text.
+	$review2 = "Best show on the CW and best new show of the fall. The path and transformation that the characters go into is shown absolutely amazing. This show has one of the best story lines in all of TV. I have never seen a poorly written or produced episode at all. The series is original and in my opinion it is flawless. The amazing casting makes this series a success and I will definitely keep watching this show for as long as it stays on. "; // Here goes the second review text.
 
 
 	include 'template_header.php';
